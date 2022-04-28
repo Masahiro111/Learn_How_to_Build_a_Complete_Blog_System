29b8dd341ab77317c97abdeb383c2b1b55933176
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-04-28 18:59:10 +0900

    finished 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Str;
@@ -26,6 +27,15 @@ class DatabaseSeeder extends Seeder
             'name' => 'author',
         ]);
 
+        Role::create([
+            'name' => 'user',
+        ]);
+
+        Category::create([
+            'name' => 'Education',
+            'slug' => 'education',
+        ]);
+
         $user = User::create([
             'name' => 'User',
             'email' => 'test@example.com',
@@ -39,7 +49,26 @@ class DatabaseSeeder extends Seeder
             'slug' => 'slug1',
             'excerpt' => 'excerpt1',
             'body' => 'body1',
+            'category_id' => 1,
         ]);
+
+        $user = User::create([
+            'name' => 'User2',
+            'email' => 'test2@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'role_id' => 2,
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user->posts()->create([
+            'title' => 'title2',
+            'slug' => 'slug2',
+            'excerpt' => 'excerpt2',
+            'body' => 'body2',
+            'category_id' => 1,
+        ]);
+
 
         // \App\Models\User::factory(10)->create();
     }
