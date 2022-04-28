0631fc5016a44cc20d655988d7adea61f996e3fb
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-04-28 17:52:19 +0900

    finished 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +18,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(
-            RoleSeeder::class
-        );
+        // $this->call(
+        //     RoleSeeder::class
+        // );
+
+        Role::create([
+            'name' => 'author',
+        ]);
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user->posts()->create([
+            'title' => 'title1',
+            'slug' => 'slug1',
+            'excerpt' => 'excerpt1',
+            'body' => 'body1',
+        ]);
 
         // \App\Models\User::factory(10)->create();
     }
