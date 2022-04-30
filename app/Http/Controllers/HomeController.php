a7d9d607b09898fff311c4d9b5860e859a52b34d
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-01 00:28:17 +0900

    23

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::query()
+        $posts = Post::query();
+
+        $posts_data = $posts
             ->withCount('comments')
             ->get();
 
-        $recents = Post::query()
+        $recents_data = $posts
             ->latest()
             ->take(5)
             ->get();
 
+        $categories = Category::query()
+            ->withCount('posts')
+            ->orderBy('posts_count', 'desc')
+            ->latest()
+            ->take(10)
+            ->get();
+
+        $tags = Tag::query()
+            ->take(50)
+            ->get();
+
         return view('home', [
-            'posts' => $posts,
-            'recents' => $recents,
+            'posts' => $posts_data,
+            'recents' => $recents_data,
+            'categories' => $categories,
+            'tags' => $tags,
         ]);
     }
 }
