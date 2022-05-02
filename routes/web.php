8d0e2c3f3cb9a601398407691fe15c5e35c334bf
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-02 19:08:03 +0900

    finished 28

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Aboutcontroller;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
@@ -26,9 +27,7 @@ Route::post('/posts/{post:slug}', [PostController::class, 'addComment'])
     ->name('post.add_comment');
 
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', Aboutcontroller::class)->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
